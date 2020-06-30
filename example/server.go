f453818e0c777bb2e85f69439ef9eba59430929f
worldflyingct <43244027+worldflyingct@users.noreply.github.com>
2020-06-30 17:00:48 +0800

    修改例子无法使用的bug.

--- example/server.go
+++ example/server.go
@@ -8,14 +8,14 @@ import (
 
 func main () {
     var in string
-	ms := go-mqttserver.StartServer(1883, 9001, "worldflying", "worldflying", []string{"loseconnect", "testtopic"}, func (topic string, d []byte) {
+	ms := mqttserver.StartServer(1883, 9001, "worldflying", "worldflying", []string{"loseconnect", "testtopic"}, func (topic string, d []byte) {
         fmt.Println(topic, string(d))
     })
     for {
         fmt.Println("SendData")
-        go-mqttserver.PublishData(ms, "testtopic", []byte("hello world"))
+        mqttserver.PublishData(ms, "testtopic", []byte("hello world"))
         time.Sleep(5*time.Second)
     }
     fmt.Scanln(&in)
-    go-mqttserver.CloseServer(ms)
+    mqttserver.CloseServer(ms)
 }
